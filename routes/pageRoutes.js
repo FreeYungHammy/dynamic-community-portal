1f3f23db263684201009ae4d582fac5f85a303cb
MercenaryTao <178577624+MercenaryTao@users.noreply.github.com>
2025-05-14 08:50:58 +0200

    Updated Data Arrays

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -5,18 +5,23 @@ const router = express.Router();
 
 // replace with real data - just dummy data for now
 const teamMembers = [
-    { name: 'Calvin Nijenhuis', role: 'Team Lead' },
-    { name: 'Brett Wilson', role: 'Backend Developer' },
-    { name: 'Danielle Janse van Vuuren', role: 'Frontend Developer' },
-    { name: 'Ruan Bath', role: 'Data Manager' },
-    { name: 'Le Marsh Vaughan', role: 'Documentation Manager' }
+    { name: 'Calvin Nijenhuis', role: 'Team Leader', studNum: 600373},
+    { name: 'Brett Wilson', role: 'Backend Developer', studNum: 601081 },
+    { name: 'Danielle Janse van Vuuren', role: 'Backend Developer', studNum: 600699},
+    { name: 'Ruan Bath', role: 'Frontend Developer', studNum:  600544},
+    { name: 'Le-Marsh Vaughan', role: 'Data Manager', studNum: 600857}
 ];
 
 // replace with real data - just dummy data for now
 const events = [
-    { title: 'Hackathon', date: '2025-05-15', location: 'Online', image: 'tech.jpg' },
-    { title: 'Art Festival', date: '2025-06-01', location: 'Stellenbosch', image: 'art.jpg' },
-    { title: 'Music Jam', date: '2025-07-20', location: 'Cape Town', image: 'music.jpg' }
+ { title: "Men's Mental Health Awareness", date: '2025-06-04', location: 'Pretoria', image: 'MMHA.jpg' },
+    { title: "CLC MIDYEAR MEETING", date: '2025-07-03', location: 'Kempton Park', image: 'CLC.jpg' },
+    { title: "CLC MIDYEAR MEETING PTA ", date: '2025-07-04', location: 'Stellenbosch', image: 'CLC.jpg' },
+    { title: "MEN'S MENTAL HEALTH AWARENESS", date: '2025-07-04', location: 'Stellenbosch', image: 'BTH.jpg' },
+    { title: "Beyond the High", date: '2025-07-22', location: 'Kempton Park', image: 'BTH.jpg' },
+    { title: "STUDENT WELLNESS EXPO", date: '2025-07-23', location: 'Pretoria', image: 'SWE.jpg' },
+    { title: "Beyond the High", date: '2025-07-29', location: 'Pretoria', image: 'BTH.jpg' },
+    { title: "STUDENT WELLNESS EXPO", date: '2025-07-30', location: 'Kempton Park', image: 'SWE.jpg' }
 ];
 
 // message storage 
