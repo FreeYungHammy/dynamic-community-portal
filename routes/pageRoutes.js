a8a62d1a723fb0b2457dc069ea8f4f993cd4e0c6
hikingtree <150736947+hikingtree@users.noreply.github.com>
2025-05-15 17:08:37 +0200

    added images, fixed pageRoutes and pages to display images, added css to display images

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -5,25 +5,22 @@ const router = express.Router();
 
 // replace with real data - just dummy data for now
 const teamMembers = [
-    { name: 'Calvin Nijenhuis', role: 'Team Leader', studNum: 600373},
-    { name: 'Brett Wilson', role: 'Backend Developer', studNum: 601081},
-    { name: 'Danielle Janse van Vuuren', role: 'Backend Developer', studNum: 600699},
-    { name: 'Ruan Bath', role: 'Frontend Developer', studNum:  600544},
-    { name: 'Le-Marsh Vaughan', role: 'Data Manager', studNum: 600857},
+    { name: 'Calvin Nijenhuis', role: 'Team Leader', studNum: 600373, image: 'calvin.png' },
+    { name: 'Brett Wilson', role: 'Backend Developer', studNum: 601081, image: 'Brett.png' },
+    { name: 'Danielle Janse van Vuuren', role: 'Backend Developer', studNum: 600699, image: 'danielle.png' },
+    { name: 'Ruan Bath', role: 'Frontend Developer', studNum: 600544, image: 'ruan.png' },
+    { name: 'Le-Marsh Vaughan', role: 'Data Manager', studNum: 600857, image: 'le-marsh.png' }
 ]
 
 // replace with real data - just dummy data for now
 const events = [
- { title: "Men's Mental Health Awareness", date: '2025-06-04', location: 'Pretoria', image: 'MMHA.jpg' },
-    { title: "CLC MIDYEAR MEETING", date: '2025-07-03', location: 'Kempton Park', image: 'CLC.jpg' },
-    { title: "CLC MIDYEAR MEETING PTA ", date: '2025-07-04', location: 'Stellenbosch', image: 'CLC.jpg' },
-    { title: "MEN'S MENTAL HEALTH AWARENESS", date: '2025-07-04', location: 'Stellenbosch', image: 'BTH.jpg' },
-    { title: "Beyond the High", date: '2025-07-22', location: 'Kempton Park', image: 'BTH.jpg' },
-    { title: "STUDENT WELLNESS EXPO", date: '2025-07-23', location: 'Pretoria', image: 'SWE.jpg' },
-    { title: "Beyond the High", date: '2025-07-29', location: 'Pretoria', image: 'BTH.jpg' },
-    { title: "STUDENT WELLNESS EXPO", date: '2025-07-30', location: 'Kempton Park', image: 'SWE.jpg' }
+    { title: "MEN'S MENTAL HEALTH AWARENESS", date: '2025-06-04', location: 'Pretoria', image: 'MMHA.jpg' },
+    { title: "CLC MIDYEAR MEETING", date: '2025-07-03', location: 'Kempton Park', image: 'CLC1.jpg' },
+    { title: "BEYOND THE HIGH", date: '2025-07-22', location: 'Kempton Park', image: 'BTH.jpg' },
+    { title: "HACKATHON", date: '2025-07-28', location: 'Pretoia', image: 'HT.jpg' },
+    { title: "CLC MIDYEAR MEETING PTA ", date: '2025-07-30', location: 'Stellenbosch', image: 'CLC2.jpg' },
+    { title: "STUDENT WELLNESS EXPO", date: '2025-08-04', location: 'Kempton Park', image: 'SWE.jpg' }
 ];
-
 // message storage 
 const messages = [];
 
